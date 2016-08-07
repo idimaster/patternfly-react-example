22dc1ef46030637d458e7d3515f01f60bd359628
Dmitrydizumskiy@eisgroup.com <idimaster@gmail.com>
2016-08-07 16:20:25 -0700

    Initial usage of vertical navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import './app.less';
 import './index.html';
+import * as jQuery from 'jquery';
+(global as any).$ = jQuery;
+(global as any).jQuery = jQuery;
 require('bootstrap');
+require('patternfly/dist/js/patternfly');
+//$().setupVerticalNavigation(true);
 
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Header } from 'patternfly-react';
+import { Header, VNavbar, Sidebar } from 'patternfly-react';
 
 const App = React.createClass({
     render: function() {
         return (<div>
-                <Header>Example</Header>
-                {this.props.children}
+                <VNavbar></VNavbar>
+                <Sidebar></Sidebar>
+                <div className="container-fluid container-cards-pf container-pf-nav-pf-vertical container-pf-nav-pf-vertical-with-sub-menus   ">
+                    <Header>Example</Header>
+                    {this.props.children}
+                </div>
             </div>
         );
     }
