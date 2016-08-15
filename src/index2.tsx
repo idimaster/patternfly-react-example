3afbd0922239fc496325f163ce1cea746bfafef0
Dmitrydizumskiy@eisgroup.com <idimaster@gmail.com>
2016-08-15 14:49:30 -0700

    VNavigationDecorator initial implementation

--- src/index2.tsx
+++ src/index2.tsx
@@ -9,14 +9,20 @@ require('patternfly/dist/js/patternfly');
 
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Header, Sidebar } from 'patternfly-react';
+import { Header, Sidebar, VNavbar } from 'patternfly-react';
 
-import { Navbar } from './Navbar'
+import { NavbarProvider } from './NavbarProvider'
 
 class App extends React.Component<any, any> {
+    provider: NavbarProvider;
+
+    componentWillMount(): void {
+        this.provider = new NavbarProvider();
+    }
+
     render(): React.ReactElement<any> {
         return (<div>
-                <Navbar/>
+                <VNavbar provider={this.provider}/>
                 <Sidebar></Sidebar>
                 <div className="container-fluid container-cards-pf container-pf-nav-pf-vertical container-pf-nav-pf-vertical-with-sub-menus   ">
                     <Header>Example</Header>
