107b1eaa8022c149dc6e842fd0ee5ac04c3cb3b1
Dmitrydizumskiy@eisgroup.com <idimaster@gmail.com>
2016-08-19 23:21:05 -0700

    VNavbar implementation

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,63 +1,51 @@
 import * as React from 'react';
 
 import { NavbarStore, VNavbar, Brand } from 'patternfly-react'
+import { NavItem, NavDropDown, NavInfoList } from 'patternfly-react'
+
 
 @VNavbar
 export class Navbar extends React.Component<any, any> implements NavbarStore {
-    getBrand(): React.ReactElement<any> {
+    getBrand() {
         return <Brand>
             <img className="navbar-brand-icon" src="/assets/img/logo-alt.svg" alt=""/><img className="navbar-brand-name" src="/assets/img/brand-alt.svg" alt="PatternFly Enterprise Application" />
         </Brand>
     }
 
-    getLeftPanelElements(): React.ReactElement<any>[] {
+    getLeftPanelElements() {
         return [
-            <li><a href="#" target="_blank" className="nav-item-iconic nav-item-iconic-new-window"><span title="Launch" className="fa fa-external-link"></span></a></li>
+            <NavItem><a href="#" target="_blank" className="nav-item-iconic nav-item-iconic-new-window"><span title="Launch" className="fa fa-external-link"></span></a></NavItem>
         ]
     }
 
-    getRightPanelElements(): React.ReactElement<any>[] {
+    getRightPanelElements() {
         return [
-            <li className="dropdown">
-                <a className="dropdown-toggle nav-item-iconic" id="notifications" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <span title="Notifications" className="fa pficon-flag"></span>
-                    <span className="badge">2</span>
-                </a>
-                <div className="dropdown-menu infotip bottom-right">
-                    <div className="arrow"></div>
-                    <ul className="list-group">
-                        <li className="list-group-item">
-                            <span className="i pficon pficon-info"></span> Modified Datasources ExampleDS
-                        </li>
-                        <li className="list-group-item">
-                            <span className="i pficon pficon-info"></span> Error: System Failure
-                        </li>
-                    </ul>
-                    <div className="footer">
-                        <a>Clear Messages</a>
-                    </div>
-                </div>
-            </li>,
-            <li className="dropdown">
-                <a className="dropdown-toggle nav-item-iconic" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <span title="Help" className="fa pficon-help"></span>
-                    <span className="caret"></span>
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <li><a href="#">Help</a></li>
-                    <li><a href="#">About</a></li>
-                </ul>
-            </li>,
-            <li className="dropdown">
-                <a className="dropdown-toggle nav-item-iconic" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    <span title="Username" className="fa pficon-user"></span>
-                    <span className="caret"></span>
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <li><a href="#">Preferences</a></li>
-                    <li><a href="#">Logout</a></li>
-                </ul>
-            </li>
-        ]
+            <NavInfoList name='Notification' store={() => {
+                return [
+                {name: 'Modified Datasources ExampleDS'},
+                {name: 'Test Message'},
+                {name: 'Error: System Failure'}
+                ];
+            }}/>,
+            <NavInfoList name='Notification' store={() => {
+                return [
+                {name: 'Modified Datasouxccc vxvc dgfgg fg  fff ff f  dfggfhdh rces ExampleDS'},
+                {name: 'Test Message'},
+                {name: 'Error: System Failure'}
+                ];
+            }}/>,
+            <NavDropDown name='Help' icon='pficon-help' store={() => {
+                return [
+                {name: 'Help'},
+                {name: 'About'}
+                ];
+            }}/>,
+            <NavDropDown name='User' icon='pficon-user' store={() => {
+                return [
+                {name: 'Preferences'},
+                {name: 'Logout'}
+                ];
+            }}/>
+        ];
     }
 }
