7544f35597ac97606025e49089ce1c280b15fb5e
Dmitry Izumskiy <idimaster@gmail.com>
2016-08-22 11:56:31 -0700

    VNavbar switch to traditional react implementation

--- src/index.tsx
+++ src/index.tsx
@@ -9,17 +9,49 @@ require('patternfly/dist/js/patternfly');
 
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Header } from 'patternfly-react';
+import { Header, VNavbar, NavItem, NavInfoList, NavDropDown, NavInfoItem, NavDropDownItem } from 'patternfly-react';
 
-import { Navbar } from './Navbar'
 import { Sidebar } from './Sidebar'
 
 class App extends React.Component<any, any> {
+
     render(): React.ReactElement<any> {
         return (<div>
-                <Navbar/>
+                <VNavbar>
+                    <VNavbar.Brand>
+                        <img className="navbar-brand-icon" src="/assets/img/logo-alt.svg" alt=""/><img
+                        className="navbar-brand-name" src="/assets/img/brand-alt.svg"
+                        alt="PatternFly Enterprise Application"/>
+                    </VNavbar.Brand>
+                    <VNavbar.LeftPanel>
+                        <NavItem><a href="#" target="_blank"
+                                    className="nav-item-iconic nav-item-iconic-new-window"><span
+                            title="Launch" className="fa fa-external-link"></span></a></NavItem>
+                    </VNavbar.LeftPanel>
+                    <VNavbar.RightPanel>
+                        <NavInfoList label='Notification'>
+                            <NavInfoItem label='Modified Datasources ExampleDS' />
+                            <NavInfoItem label='Test Message' />
+                            <NavInfoItem label='Error: System Failure' />
+                        </NavInfoList>
+                        <NavInfoList label='Notification'>
+                            <NavInfoItem label='Modified Datasouxccc vxvc dgfgg fg  fff ff f  dfggfhdh rces ExampleDS' />
+                            <NavInfoItem label='Test Message' />
+                            <NavInfoItem label='Error: System Failure' />
+                        </NavInfoList>
+                        <NavDropDown name='Help' icon='pficon-help'>
+                            <NavDropDownItem label='Help'/>
+                            <NavDropDownItem label='About'/>
+                        </NavDropDown>
+                        <NavDropDown name='User' icon='pficon-user'>
+                            <NavDropDownItem label='Preferences'/>
+                            <NavDropDownItem label='Logout'/>
+                        </NavDropDown>
+                    </VNavbar.RightPanel>
+                </VNavbar>
                 <Sidebar></Sidebar>
-                <div className="container-fluid container-cards-pf container-pf-nav-pf-vertical container-pf-nav-pf-vertical-with-sub-menus   ">
+                <div
+                    className="container-fluid container-cards-pf container-pf-nav-pf-vertical container-pf-nav-pf-vertical-with-sub-menus   ">
                     <Header>Example</Header>
                     {this.props.children}
                 </div>
@@ -27,6 +59,7 @@ class App extends React.Component<any, any> {
         );
     }
 }
+
 
 render(<App/>, document.getElementById('app-container'));
 
