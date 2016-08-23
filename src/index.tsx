db0886fd171d08407a7281537c695e6b31bdb6f8
Dmitry Izumskiy <idimaster@gmail.com>
2016-08-22 17:23:31 -0700

    Added event and routing support

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,21 @@ require('bootstrap/js/popover');
 require('patternfly/dist/js/patternfly');
 
 import * as React from 'react';
-import { render } from 'react-dom';
-import { Header, VNavbar, NavItem, NavInfoList, NavDropDown, NavInfoItem, NavDropDownItem } from 'patternfly-react';
-import { Sidebar, SidebarItem, SidebarItemList, SidebarListItem } from 'patternfly-react';
+import {render} from 'react-dom';
+import {Header, VNavbar, NavItem, NavInfoList, NavDropDown, NavInfoItem, NavDropDownItem} from 'patternfly-react';
+import {Sidebar, SidebarItem, SidebarItemList, SidebarListItem} from 'patternfly-react';
+import {CommandPayload} from 'patternfly-react';
+
 
 class App extends React.Component<any, any> {
+
+    private handleTopMenu = (event: CommandPayload) => {
+        alert(event.command);
+    };
+
+    private handleClear = (id: string) => {
+        alert('Clear ' + id);
+    };
 
     render(): React.ReactElement<any> {
         return (<div>
@@ -28,47 +38,47 @@ class App extends React.Component<any, any> {
                             title="Launch" className="fa fa-external-link"></span></a></NavItem>
                     </VNavbar.LeftPanel>
                     <VNavbar.RightPanel>
-                        <NavInfoList label='Notification'>
+                        <NavInfoList id='id1' label='Notification' onClear={this.handleClear}>
                             <NavInfoItem label='Modified Datasources ExampleDS' />
                             <NavInfoItem label='Test Message' />
                             <NavInfoItem label='Error: System Failure' />
                         </NavInfoList>
-                        <NavInfoList label='Notification'>
+                        <NavInfoList id='id2' label='Notification' onClear={this.handleClear}>
                             <NavInfoItem label='Modified Datasouxccc vxvc dgfgg fg  fff ff f  dfggfhdh rces ExampleDS' />
                             <NavInfoItem label='Test Message' />
                             <NavInfoItem label='Error: System Failure' />
                         </NavInfoList>
                         <NavDropDown name='Help' icon='pficon-help'>
-                            <NavDropDownItem label='Help'/>
-                            <NavDropDownItem label='About'/>
+                            <NavDropDownItem label='Help' onSelect={this.handleTopMenu} payload={{command: 'help'}}/>
+                            <NavDropDownItem label='About' onSelect={this.handleTopMenu} payload={{command: 'about'}}/>
                         </NavDropDown>
                         <NavDropDown name='User' icon='pficon-user'>
-                            <NavDropDownItem label='Preferences'/>
-                            <NavDropDownItem label='Logout'/>
+                            <NavDropDownItem label='Preferences' onSelect={this.handleTopMenu} payload={{command: 'preferences'}}/>
+                            <NavDropDownItem label='Logout' onSelect={this.handleTopMenu} payload={{command: 'logout'}}/>
                         </NavDropDown>
                     </VNavbar.RightPanel>
                 </VNavbar>
                 <Sidebar>
-                    <SidebarItem icon='fa-dashboard' label='Dashboard'/>
-                    <SidebarItem icon='fa-shield' label='Dolor'/>
+                    <SidebarItem icon='fa-dashboard' label='Dashboard' route='#1'/>
+                    <SidebarItem icon='fa-shield' label='Dolor' route='#2'/>
                     <SidebarItemList icon='fa-space-shuttle' label='Ipsum'>
-                        <SidebarListItem label='Intellegam'/>
-                        <SidebarListItem label='Copiosae'/>
-                        <SidebarListItem label='Patrioque'/>
+                        <SidebarListItem label='Intellegam' route='#31'/>
+                        <SidebarListItem label='Copiosae' route='#32'/>
+                        <SidebarListItem label='Patrioque' route='#33'/>
                     </SidebarItemList>
                     <SidebarItemList icon='fa-paper-plane' label='Amet'>
-                        <SidebarListItem label='A Intellegam'/>
-                        <SidebarListItem label='A Copiosae'/>
-                        <SidebarListItem label='A Patrioque'/>
+                        <SidebarListItem label='A Intellegam' route='#2'/>
+                        <SidebarListItem label='A Copiosae' route='#2'/>
+                        <SidebarListItem label='A Patrioque' route='#2'/>
 
                     </SidebarItemList>
                     <SidebarItemList icon='fa-space-shuttle' label='Ipsum3'>
-                        <SidebarListItem label='I Intellegam'/>
-                        <SidebarListItem label='I Copiosae'/>
-                        <SidebarListItem label='I Patrioque'/>
+                        <SidebarListItem label='I Intellegam' route='#2'/>
+                        <SidebarListItem label='I Copiosae' route='#2'/>
+                        <SidebarListItem label='I Patrioque' route='#2'/>
                     </SidebarItemList>
-                    <SidebarItem icon='fa-graduation-cap' label='Adipscing'/>
-                    <SidebarItem icon='fa-gamepad' label='Lorem'/>
+                    <SidebarItem icon='fa-graduation-cap' label='Adipscing' route='#2'/>
+                    <SidebarItem icon='fa-gamepad' label='Lorem' route='#2'/>
                 </Sidebar>
                 <div
                     className="container-fluid container-cards-pf container-pf-nav-pf-vertical container-pf-nav-pf-vertical-with-sub-menus   ">
