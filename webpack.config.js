4309665f5654273faa89a13c090dfbb39ead333b
Dmitry Izumskiy <idimaster@gmail.com>
2017-02-08 12:56:41 -0700

    Update examples to latest version

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, 'docs'),
-        publicPath: '/',
+        publicPath: '/patternfly-react-example/',
         filename: 'bundle.js'
     },
     module: {
