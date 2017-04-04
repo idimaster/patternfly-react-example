0333bdc5c58ecf689ffbd6b7c33f6cc8cd738076
Dmitry Izumskiy <idimaster@gmail.com>
2017-04-04 13:40:18 -0700

    Update examples to latest version, draft Wizard implementation

--- src/wizard/WizardExample.tsx
+++ src/wizard/WizardExample.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
 
+import {Wizard, WizardBody, WizardSteps, WizardButtons, Title, WizardRow, WizardSidebar, WizardMain, StepTitle} from 'patternfly-react'
 
 export class WizardExample extends React.Component<any, any> {
     render(): React.ReactElement<any> {
-        return null
+        return <Wizard>
+            <WizardBody>
+                <WizardSteps>
+                    <Title number='1' title='First Step' />
+                    <Title number='2' title='Second Step' />
+                    <Title number='3' title='Review' />
+                </WizardSteps>
+                <WizardRow>
+                    <WizardSidebar>
+                        <StepTitle number='1A.' title='Details'/>
+                        <StepTitle number='1B.' title='Settings'/>
+                    </WizardSidebar>
+                    <WizardMain>
+
+                    </WizardMain>
+                </WizardRow>
+            </WizardBody>
+            <WizardButtons></WizardButtons>
+        </Wizard>
     }
 }
